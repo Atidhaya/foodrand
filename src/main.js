8a3edabb7145606cc346b53dfad7cbb9793362db
EFPs <st_18912@hotmail.com>
2018-06-02 14:00:33 +0700

    Working page transition (Home Create Join), Home page ui (Except view members)

--- src/main.js
+++ src/main.js
@@ -14,6 +14,10 @@ import Framework7Styles from 'framework7/dist/css/framework7.css';
 import IconsStyles from './css/icons.css';
 import AppStyles from './css/app.css';
 
+//Import Vuefire
+import VueFire from 'vuefire';
+
+
 // Import Routes
 import Routes from './routes.js'
 
@@ -23,6 +27,9 @@ import SignIn from './pages/sign-in.vue'
 
 // Init F7 Vue Plugin
 Vue.use(Framework7Vue, Framework7)
+
+// Init VueFire
+Vue.use(VueFire)
 
 // Init App
 new Vue({
@@ -38,7 +45,7 @@ new Vue({
   },
   // Register App Component
   components: {
-    app: SignIn
-    // app: App
+    // app: SignIn
+    app: App
   }
 });
